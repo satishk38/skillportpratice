90bab0ff99d6a590889cd344d3da12440fde61f0
satishk38 <satishk38@gmail.com>
2022-07-05 19:45:11 -0400

    new version

---
+++ mywebapp/src/main/java/com/mycompany/web/EchoParameterValueServlet.java
@@ -0,0 +1,23 @@
+package com.mycompany.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EchoParameterValueServlet extends HttpServlet {
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	    throws ServletException, IOException{
+		
+		String echo = req.getParameter("echo");
+		resp.getWriter().println(echo);
+		resp.setStatus(HttpServletResponse.SC_OK);
+		
+	}
+
+}
